f31c619625751985923c4cf9b5ccba77b7bafc95
gerryron <gerryron02@gmail.com>
2022-10-23 15:57:58 +0700

    templating & setup database oke

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+@include('layouts.admin.partials._header')
+<body class="hold-transition skin-blue sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+
+@include('layouts.admin.partials._navbar')
+
+@include('layouts.admin.partials._sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Blank page
+        <small>it all starts here</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Examples</a></li>
+        <li class="active">Blank page</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      @yield('content')
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  @include('layouts.admin.partials._footer')
+
+  @include('layouts.admin.partials._asidecontrol')
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+@include('layouts.admin.partials._script')
+</body>
+</html>
